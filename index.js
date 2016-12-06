6b1bb9d8f27d10f30536ae5f564dde89a733fff2
Miklos Bertalan <miklos.bertalan@risingstack.com>
2016-12-06 13:34:11 +0100

    feat(core): add supported flag to NX

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 'use strict'
 
-require('./polyfills')
+var nx
 
-const nx = {
-  component: require('./core'),
-  middlewares: require('./middlewares'),
-  components: require('./components'),
-  filters: require('./filters'),
-  limiters: require('./limiters'),
-  observer: require('@risingstack/nx-observe'),
-  compiler: require('@risingstack/nx-compile')
+if (typeof Proxy === 'undefined') {
+  nx = { supported: false }
+} else {
+  require('./polyfills')
+
+  nx = {
+    component: require('./core'),
+    middlewares: require('./middlewares'),
+    components: require('./components'),
+    filters: require('./filters'),
+    limiters: require('./limiters'),
+    observer: require('@risingstack/nx-observe'),
+    compiler: require('@risingstack/nx-compile'),
+    supported: true
+  }
+  for (let name in nx.filters) {
+    nx.middlewares.expression.filter(name, nx.filters[name])
+  }
+  for (let name in nx.limiters) {
+    nx.middlewares.code.limiter(name, nx.limiters[name])
+  }
 }
 
-for (let name in nx.filters) {
-  nx.middlewares.expression.filter(name, nx.filters[name])
-}
-
-for (let name in nx.limiters) {
-  nx.middlewares.code.limiter(name, nx.limiters[name])
-}
-
-if (module && module.exports) {
+if (typeof module !== 'undefined' && module.exports) {
   module.exports = nx
 }
-if (window) {
+if (typeof window !== 'undefined') {
   window.nx = nx
 }
