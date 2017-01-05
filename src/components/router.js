2cfdaa97b5c41578a8096645f8021f4a05e69bbb
Miklos Bertalan <miklos.bertalan@risingstack.com>
2017-01-05 10:49:00 +0100

    feat(router): change router config to be stateless by default

--- src/components/router.js
+++ src/components/router.js
@@ -4,6 +4,8 @@ const component = require('@nx-js/core')
 const middlewares = require('../middlewares')
 
 module.exports = function routerComp (config) {
+  config = Object.assign({state: false}, config)
+
   return component(config)
     .use(middlewares.route)
 }
